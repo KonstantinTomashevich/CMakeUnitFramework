4606354d7d4c24a63a92a5a28a9b5477504da295
Konstantin Tomashevich <konstantin.tomashevich@gmail.com>
2025-06-21 19:19:04 +0300

    Cushion: Enable new features.

--- Unit.cmake
+++ Unit.cmake
@@ -674,7 +674,7 @@ function (concrete_preprocessing_queue_step_cushion)
                 COMMAND
                 "cushion"
                 "--options" "forbid-macro-redefinition"
-                "--features" "defer" "wrapper-macro" "statement-accumulator"
+                "--features" "defer" "wrapper-macro" "statement-accumulator" "snippet" "evaluated-argument"
                 "--input" "${STEP_SOURCE}"
                 "--output" "${STEP_OUTPUT}"
                 "--cmake-depfile" "${STEP_OUTPUT}.deps"
